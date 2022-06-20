19c31911f532b62161cca780d8783c43b9ea5155
Alan Samuel Martinski <59567093+amartinski@users.noreply.github.com>
2022-06-20 11:52:46 -0300

    Add files via upload

---
+++ anyupdate.sh
@@ -0,0 +1,7 @@
+#sudo wget -O- https://gitlab.com/wilker.ribeiro/scripts/-/raw/main/update_services_vs_food_V1.2.sh | bash
+
+cd /tmp
+wget -c https://github.com/amartinski/anydeskupdate/blob/main/anydesk_6.1.1-1_amd64%20(1).deb -O anydesk_6.1.1-1_amd64%20(1).deb
+
+anydesk_6.1.1-1_amd64%20(1).deb
+

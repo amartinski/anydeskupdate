c7b8db2135c2d70984c9f2c7f9aa576d66ede3f8
Alan Samuel Martinski <59567093+amartinski@users.noreply.github.com>
2022-06-20 14:32:23 -0300

    Add files via upload

--- anydesk.sh
+++ anydesk.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash
 cd /tmp
-wget -c https://github.com/amartinski/anydeskupdate/raw/main/anydesk_6.1.1-1_amd64.deb -O sudo 
-sudo apt install ./anydesk_6.1.1-1_amd64.deb -y
+wget -c https://github.com/amartinski/anydeskupdate/raw/main/anydesk_6.1.1-1_amd64.deb
+sudo apt install anydesk/anydesk_6.1.1-1_amd64.deb -y

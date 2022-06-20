004444214ee8b46104fb0babe4304fb8fb80c581
Alan Samuel Martinski <59567093+amartinski@users.noreply.github.com>
2022-06-20 13:35:46 -0300

    Update anyupdate.sh

--- anyupdate.sh
+++ anyupdate.sh
@@ -1,5 +1,5 @@
 #sudo wget -O- https://raw.githubusercontent.com/amartinski/anydeskupdate/main/anyupdate.sh | bash
 
 cd /tmp
-wget -c https://github.com/amartinski/anydeskupdate/raw/main/anydesk_6.1.1-1_amd64.deb -O anydesk_6.1.1-1_amd64%20.deb
-anydesk_6.1.1-1_amd64%20.deb
+wget -c https://github.com/amartinski/anydeskupdate/raw/main/anydesk_6.1.1-1_amd64.deb -O 
+sudo dpkg -i anydesk_6.1.1-1_amd64.deb

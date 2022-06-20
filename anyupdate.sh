471fff9da564c7a57ba7332d164d5053509c85b5
Alan Samuel Martinski <59567093+amartinski@users.noreply.github.com>
2022-06-20 13:47:55 -0300

    Add files via upload

--- anyupdate.sh
+++ anyupdate.sh
@@ -1,5 +1,31 @@
 #sudo wget -O- https://raw.githubusercontent.com/amartinski/anydeskupdate/main/anyupdate.sh | bash
 
 cd /tmp
-wget -c https://github.com/amartinski/anydeskupdate/raw/main/anydesk_6.1.1-1_amd64.deb -O sudo 
-sudo apt install ./anydesk_6.1.1-1_amd64.deb -y
+
+# Download instalação Anydesk
+
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+
+# Repositorio 
+
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+
+# update apt cache
+
+apt update
+
+#Install anydesk
+
+apt install anydesk
+
+# Download Team viewer
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+
+# Run the news installers
+
+sudo dpkg -i teamviewer_amd64.deb
+
+# Instalar
+
+sudo apt-get -f install

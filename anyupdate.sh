b2329a48b44fec95200bd1fd2249cc7a3f42b707
Alan Samuel Martinski <59567093+amartinski@users.noreply.github.com>
2022-06-20 12:01:43 -0300

    Update anyupdate.sh

--- anyupdate.sh
+++ anyupdate.sh
@@ -1,7 +1,5 @@
-#sudo wget -O- https://gitlab.com/wilker.ribeiro/scripts/-/raw/main/update_services_vs_food_V1.2.sh | bash
+#sudo wget -O- https://raw.githubusercontent.com/amartinski/anydeskupdate/main/anyupdate.sh | bash
 
 cd /tmp
-wget -c https://github.com/amartinski/anydeskupdate/blob/main/anydesk_6.1.1-1_amd64%20(1).deb -O anydesk_6.1.1-1_amd64%20(1).deb
-
-anydesk_6.1.1-1_amd64%20(1).deb
-
+wget -c https://github.com/amartinski/anydeskupdate/raw/main/anydesk_6.1.1-1_amd64.deb -O anydesk_6.1.1-1_amd64%20.deb
+anydesk_6.1.1-1_amd64%20.deb
